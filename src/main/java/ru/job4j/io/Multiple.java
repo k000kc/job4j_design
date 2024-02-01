0642cd0c576e8279f6dbee1886b4ede61af035f8
Andrey <ham2188@mail.ru>
2024-02-01 19:32:20 +0300

    3. Статистика по коллекции. [#45889]

---
+++ src/main/java/ru/job4j/io/Multiple.java
@@ -0,0 +1,16 @@
+package ru.job4j.io;
+
+import java.io.FileOutputStream;
+
+public class Multiple {
+    public static void main(String[] args) {
+        try (FileOutputStream output = new FileOutputStream("data/dataresult.txt")) {
+            for (int i = 0; i < 10; i++) {
+                output.write(("1 * " + i + " = " + 1 * i).getBytes());
+                output.write(System.lineSeparator().getBytes());
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
