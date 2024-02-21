99823ad48dfab155604f2b5c24d0248f23c870de
Andrey <ham2188@mail.ru>
2024-02-21 21:55:48 +0300

    11. Поиск дубликатов [#315066]

---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesFinder.java
@@ -0,0 +1,18 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class DuplicatesFinder {
+    public static void main(String[] args) throws IOException {
+        DuplicatesVisitor visitor = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), visitor);
+        visitor.getFiles().forEach((k, v) -> {
+            if (v.size() > 1) {
+                System.out.printf("%s - %d byte\n", k.getName(), k.getSize());
+                v.forEach(System.out::println);
+            }
+        });
+    }
+}
---
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -0,0 +1,28 @@
+package ru.job4j.io.duplicates;
+
+import java.io.IOException;
+import java.nio.file.FileVisitResult;
+import java.nio.file.Path;
+import java.nio.file.SimpleFileVisitor;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.*;
+
+public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
+    private Map<FileProperty, List<Path>> files = new HashMap<>();
+
+    public Map<FileProperty, List<Path>> getFiles() {
+        return files;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
+        if (files.containsKey(fileProperty)) {
+            files.get(fileProperty).add(file);
+        } else {
+            files.put(fileProperty, new ArrayList<>());
+            files.get(fileProperty).add(file);
+        }
+        return super.visitFile(file, attrs);
+    }
+}
