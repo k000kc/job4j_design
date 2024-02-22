067823ed06acb9e7c9137b40ffe5436b963f3056
Andrey <ham2188@mail.ru>
2024-02-22 22:37:14 +0300

    11. Поиск дубликатов [#315066] - fix

--- src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/duplicates/DuplicatesVisitor.java
@@ -17,10 +17,7 @@ public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
-        files.computeIfAbsent(fileProperty, k ->
-                files.get(k) == null ? new ArrayList<>() : files.get(k)
-        ).add(file);
+        files.computeIfAbsent(fileProperty, k -> new ArrayList<>()).add(file);
         return super.visitFile(file, attrs);
-
     }
 }
