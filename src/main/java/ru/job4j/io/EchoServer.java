ef094a42512c107a9cabd18f645f0e325a624b06
Andrey <ham2188@mail.ru>
2024-03-31 13:43:02 +0300

    3. Бот [#7921]

--- src/main/java/ru/job4j/io/EchoServer.java
+++ src/main/java/ru/job4j/io/EchoServer.java
@@ -6,6 +6,9 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Objects;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class EchoServer {
     public static void main(String[] args) throws IOException {
@@ -16,18 +19,30 @@ public class EchoServer {
                      BufferedReader input = new BufferedReader(
                              new InputStreamReader(socket.getInputStream()))) {
                     output.write("HTTP/1.1 200 OK\r\n\r\n".getBytes());
-                    String string = input.readLine();
-                    if (string.contains("msg=Bye")) {
-                        socket.close();
+                    String answer = prepareAnswer(input.readLine());
+                    if (Objects.equals(answer, "Exit")) {
+                        output.write("Shut down the server".getBytes());
                         server.close();
+                        break;
                     }
-                    while (string != null && !string.isEmpty()) {
-                        System.out.println(string);
-                        string = input.readLine();
+                    if (Objects.equals(answer, "Hello")) {
+                        output.write(answer.getBytes());
+                    } else {
+                        output.write("What".getBytes());
                     }
                     output.flush();
                 }
             }
         }
+    }
+
+    private static String prepareAnswer(String line) {
+        String rsl = "";
+        Pattern pattern = Pattern.compile("((?<=msg=).*?(?= HTTP))");
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.find()) {
+            rsl = matcher.group();
+        }
+        return rsl;
     }
 }
