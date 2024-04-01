c99e4b930f013201a10307822f4b1304d842d076
Andrey <ham2188@mail.ru>
2024-04-01 20:36:02 +0300

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -12,5 +12,18 @@ public class UsageLog4j {
         LOG.info("info message");
         LOG.warn("warn message");
         LOG.error("error message");
+
+        String name = "Andrey Petrov";
+        byte age = 35;
+        char gender = 'M';
+        float height = 170.5F;
+        short numBricks = 25000;
+        double numBacteria = 1.2E+09;
+        double spaceDistance = 1.432E+12;
+        boolean isError = false;
+        long fileSize = 4096L;
+        LOG.debug("User info name: {}, age: {}, gender: {}, height: {}F", name, age, gender, height);
+        LOG.debug("number of bricks: {}, size of bacterium: {}, space distance: {}", numBricks, numBacteria, spaceDistance);
+        LOG.debug("the presence of an error: {}, file size: {}L", isError, fileSize);
     }
 }
