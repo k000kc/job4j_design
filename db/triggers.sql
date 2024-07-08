ea8d9e0e3eed3ffa0ff737f5bf7bdb83645d9102
Andrey <ham2188@mail.ru>
2024-07-08 21:11:55 +0300

    2. Триггеры [#504803]

---
+++ db/triggers.sql
@@ -0,0 +1,70 @@
+create table products
+(
+	id serial primary key,
+    	name varchar(50),
+    	producer varchar(50),
+    	count integer default 0,
+    	price integer
+);
+
+create or replace function tax()
+returns trigger as $$
+BEGIN
+	new.price := new.price + new.price * 0.13;
+	RETURN NEW;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+/*
+Триггер должен срабатывать после вставки данных, 
+для любого товара и просто насчитывать налог на товар 
+(нужно прибавить налог к цене товара). Действовать он 
+должен не на каждый ряд, а на запрос (statement уровень)
+*/
+
+create or replace trigger tax_trigger_after
+after insert on products
+referencing new table as inserted
+for each statement
+execute function tax();
+
+/*
+Триггер должен срабатывать до вставки данных и насчитывать 
+налог на товар (нужно прибавить налог к цене товара). 
+Здесь используем row уровень.
+*/
+
+create or replace trigger tax_trigger_before
+before insert on products
+for each row
+execute function tax(); 
+
+
+/*
+Нужно написать триггер на row уровне, который сразу после вставки продукта в таблицу products, 
+будет заносить имя, цену и текущую дату в таблицу history_of_price
+*/
+
+create table history_of_price
+(
+	id serial primary key,
+    	name varchar(50),
+    	price integer,
+    	date timestamp
+);
+
+create or replace function write_history()
+returns trigger as $$
+BEGIN
+	insert into history_of_price(name, price, date)
+	values(new.name, new.price, current_timestamp);
+	return new;
+END;
+$$
+LANGUAGE 'plpgsql';
+
+create trigger insert_trigger_after
+after insert on products
+for each row
+execute function write_history();
