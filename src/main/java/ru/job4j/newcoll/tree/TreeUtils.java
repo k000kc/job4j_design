5bc82489d40f2f274383a0f4272f0f290e22286a
Andrey <ham2188@mail.ru>
2025-06-04 21:56:24 +0300

    Обход дерева в глубину [#504974]

--- src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
+++ src/main/java/ru/job4j/newcoll/tree/TreeUtils.java
@@ -1,9 +1,6 @@
 package ru.job4j.newcoll.tree;
 
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Queue;
+import java.util.*;
 
 public class TreeUtils<T> {
     /**
@@ -48,5 +45,72 @@ public class TreeUtils<T> {
             queue.addAll(currentNode.getChildren());
         }
         return keys;
+    }
+
+    /**
+     * Метод обходит дерево root и добавляет к узлу с ключом parent
+     * новый узел с ключом child, при этом на момент добавления узел с ключом parent
+     * уже должен существовать в дереве, а узла с ключом child в дереве быть не должно
+     * @param root корень дерева
+     * @param parent ключ узла-родителя
+     * @param child ключ узла-потомка
+     * @return true если добавление произошло успешно и false в обратном случае.
+     * @throws IllegalArgumentException если root является null
+     */
+    public boolean add(Node<T> root, T parent, T child) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        boolean result = false;
+        Optional<Node<T>> parentOptional = findByKey(root, parent);
+        Optional<Node<T>> childOptional = findByKey(root, child);
+        if (parentOptional.isPresent() && childOptional.isEmpty()) {
+            parentOptional.get().getChildren().add(new Node<>(child));
+            result = true;
+        }
+        return result;
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key
+     * @param root корень дерева
+     * @param key ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    public Optional<Node<T>> findByKey(Node<T> root, T key) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        Stack<Node<T>> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()) {
+            Node<T> current = stack.pop();
+            if (current.getValue() != null && current.getValue().equals(key)) {
+                return Optional.of(current);
+            }
+            if (current.getChildren() != null) {
+                stack.addAll(current.getChildren());
+            }
+        }
+        return Optional.empty();
+    }
+
+    /**
+     * Метод обходит дерево root и возвращает первый найденный узел с ключом key,
+     * при этом из дерева root удаляется все поддерево найденного узла
+     * @param root корень дерева
+     * @param key ключ поиска
+     * @return узел с ключом key, завернутый в объект типа Optional
+     * @throws IllegalArgumentException если root является null
+     */
+    public Optional<Node<T>> divideByKey(Node<T> root, T key) {
+        if (root == null) {
+            throw new IllegalArgumentException();
+        }
+        Optional<Node<T>> foundOptional = findByKey(root, key);
+        Node<T> foundNode = foundOptional.get();
+        root.getChildren().removeIf(child -> child.equals(foundNode));
+        return foundOptional;
     }
 }
