eec805f8fd5037431a1a91964a41d4ac72343519
Andrey <ham2188@mail.ru>
2025-06-17 20:45:47 +0300

    Бинарное дерево поиска. Методы добавления, поиска и обхода дерева. [#504983]

---
+++ src/main/java/ru/job4j/collection/binarytree/BinarySearchTree.java
@@ -0,0 +1,171 @@
+package ru.job4j.collection.binarytree;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+public class BinarySearchTree<T extends Comparable<T>> {
+    private Node root;
+
+    public boolean put(T key) {
+        boolean result;
+        if (Objects.isNull(root)) {
+            root = new Node(key);
+            result = true;
+        } else {
+            result = put(root, key);
+        }
+        return result;
+    }
+
+    private boolean put(Node node, T key) {
+        boolean result;
+        int compair = key.compareTo(node.key);
+        if (compair == 0) {
+            result = false;
+        }
+        if (compair < 0) {
+            if (node.getLeft() == null) {
+                node.left = new Node(key);
+                result = true;
+            } else {
+                result = put(node.left, key);
+            }
+        } else {
+            if (node.right == null) {
+                node.right = new Node(key);
+                result = true;
+            } else {
+                result = put(node.right, key);
+            }
+        }
+        return result;
+    }
+
+    public boolean contains(T key) {
+        boolean result = false;
+        Node node = find(root, key);
+        if (node != null) {
+            result = true;
+        }
+        return result;
+    }
+
+    private Node find(Node node, T key) {
+        Node result;
+        if (node == null) {
+            return null;
+        }
+        int compair = key.compareTo(node.key);
+        if (compair == 0) {
+            result = node;
+        } else if (compair < 0) {
+            result = find(node.left, key);
+        } else {
+            result = find(node.right, key);
+        }
+        return result;
+    }
+
+    public boolean remove(T key) {
+        /* Метод будет реализован в следующих уроках */
+        return false;
+    }
+
+    public List<T> inSymmetricalOrder() {
+        List<T> result = new ArrayList<>();
+        Node node = root;
+        return inSymmetricalOrder(node, result);
+    }
+
+    private List<T> inSymmetricalOrder(Node localRoot, List<T> list) {
+        if (localRoot != null) {
+            inSymmetricalOrder(localRoot.left, list);
+            list.add(localRoot.key);
+            inSymmetricalOrder(localRoot.right, list);
+        }
+        return list;
+    }
+
+    public List<T> inPreOrder() {
+        List<T> result = new ArrayList<>();
+        Node node = root;
+        return inPreOrder(node, result);
+    }
+
+    private List<T> inPreOrder(Node localRoot, List<T> list) {
+        if (localRoot != null) {
+            list.add(localRoot.key);
+            inPreOrder(localRoot.left, list);
+            inPreOrder(localRoot.right, list);
+        }
+        return list;
+    }
+
+    public List<T> inPostOrder() {
+        List<T> result = new ArrayList<>();
+        Node node = root;
+        return inPostOrder(node, result);
+    }
+
+    private List<T> inPostOrder(Node localRoot, List<T> list) {
+        if (localRoot != null) {
+            inPostOrder(localRoot.left, list);
+            inPostOrder(localRoot.right, list);
+            list.add(localRoot.key);
+        }
+        return list;
+    }
+
+    public T minimum() {
+        return Objects.nonNull(root) ? minimum(root).key : null;
+    }
+
+    private Node minimum(Node node) {
+        if (node.left == null) {
+            return node;
+        }
+        return minimum(node.left);
+    }
+
+    public T maximum() {
+        return Objects.nonNull(root) ? maximum(root).key : null;
+    }
+
+    private Node maximum(Node node) {
+        if (node.right == null) {
+            return node;
+        }
+        return maximum(node.right);
+    }
+
+    @Override
+    public String toString() {
+        return PrintTree.getTreeDisplay(root);
+    }
+
+    private class Node implements VisualNode {
+        private T key;
+        private Node left;
+        private Node right;
+
+        public Node(T key) {
+            this.key = key;
+        }
+
+        @Override
+        public VisualNode getLeft() {
+            return left;
+        }
+
+        @Override
+        public VisualNode getRight() {
+            return right;
+        }
+
+        @Override
+        public String getText() {
+            return key.toString();
+        }
+    }
+}
